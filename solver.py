072fbf8bbd457cc72b5f5dab0e9016b04c0a9408
jaye1944 <jaye1944@gmail.com>
2013-08-14 21:53:08 +0530

    print solution

--- solver.py
+++ solver.py
@@ -2,23 +2,23 @@ def slove(maze):
     down = len(maze)-1
     cross = len(maze[0])-1
     while True:
-        change = True
+        not_change = True
         for i in range(1,down):
             for j in range(1,cross):
                 k = maze[i][j]
                 if k == '.':
                     if ((maze[i][j-1]=='#' or maze[i][j-1]=='b') and (maze[i-1][j]=='#' or maze[i-1][j]=='b') and (maze[i][j+1]=='#' or maze[i][j+1]=='b')):
                         maze[i][j] = 'b'
-                        change = False
+                        not_change = False
                     elif ((maze[i-1][j]=='#' or maze[i-1][j]=='b') and (maze[i][j+1]=='#' or maze[i][j+1]=='b') and (maze[i+1][j]=='#' or maze[i+1][j]=='b')):
                         maze[i][j] = 'b'
-                        change = False
+                        not_change = False
                     elif ((maze[i][j+1]=='#' or maze[i][j+1]=='b') and (maze[i+1][j]=='#' or maze[i+1][j]=='b') and (maze[i][j-1]=='#' or maze[i][j-1]=='b')):
                         maze[i][j] = 'b'
-                        change = False
+                        not_change = False
                     elif ((maze[i+1][j]=='#' or maze[i+1][j]=='b') and (maze[i][j-1]=='#' or maze[i][j-1]=='b') and (maze[i-1][j]=='#' or maze[i-1][j]=='b')):
                         maze[i][j] = 'b'
-                        change = False
-        if change:
+                        not_change = False
+        if not_change:
             break
     return maze
